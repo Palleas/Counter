5eb83ef51cc6725628c3f9cac9c0ab55bb08ff67
Romain Pouclet <romain.pouclet@gmail.com>
2017-01-31 10:02:26 -0500

    Initial commit

---
+++ YouCanDoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouCanDoIt
+//
+//  Created by POUCLET, Romain (MTL) on 2017-01-31.
+//  Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
