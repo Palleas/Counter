50ef31969094ccebb49dcfd46d2552b7b743c3f9
Romain Pouclet <romain.pouclet@gmail.com>
2017-01-31 22:48:18 -0500

    Add debug for each file

--- Counter/Counter.swift
+++ Counter/Counter.swift
@@ -15,7 +15,7 @@ final class Counter {
     }
 
     func count() -> SignalProducer<Int, CounterError> {
-        return fetch().map { content in
+        return fetch().map { name, content in
             let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation, .joinNames]
             let schemes = NSLinguisticTagger.availableTagSchemes(forLanguage: "en")
             let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
@@ -23,28 +23,29 @@ final class Counter {
 
             let tags = tagger.tags(in: NSMakeRange(0, (content as NSString).length), scheme: NSLinguisticTagSchemeTokenType, options: options, tokenRanges: nil)
 
+            print("\(name): \(tags.count) words")
             return tags.count
         }
         .reduce(0, +)
     }
 
-    func fetch() -> SignalProducer<String, CounterError> {
+    func fetch() -> SignalProducer<(String, String), CounterError> {
         return c
             .content(atPath: "_drafts", in: Repository(owner: "palleas", name: "romain-pouclet.com"))
             .mapError { CounterError.githubError(wrapped: $0) }
-            .flatMap(.latest) { response, content -> SignalProducer<String, CounterError> in
+            .flatMap(.latest) { response, content -> SignalProducer<(String, String), CounterError> in
                 guard case let .directory(files) = content else {
                     return SignalProducer(error: .invalidDirectory)
                 }
 
                 return SignalProducer(files)
-                    .flatMap(.concat) { file -> SignalProducer<String, CounterError> in
+                    .flatMap(.concat) { file -> SignalProducer<(String, String), CounterError> in
                         guard case let .file(_, url) = file.content, let downloadUrl = url else { return .empty }
 
                         let download = URLSession.shared.reactive.data(with: URLRequest(url: downloadUrl))
                             .mapError { _ in CounterError.downloadError }
                             .map { data, _ in data }
-                            .map { String(data: $0, encoding: .utf8)! }
+                            .map { (file.name, String(data: $0, encoding: .utf8)!) }
 
                         return download
                     }
